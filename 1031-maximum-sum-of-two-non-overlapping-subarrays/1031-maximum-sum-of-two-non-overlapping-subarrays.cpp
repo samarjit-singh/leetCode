3531fd65a2ed8e68a171851e6b336c1efb42767e
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-06-06 15:48:24 +0530

    Time: 8 ms (54.87%), Space: 8.3 MB (96.05%) - LeetHub

---
+++ 1031-maximum-sum-of-two-non-overlapping-subarrays/1031-maximum-sum-of-two-non-overlapping-subarrays.cpp
@@ -0,0 +1,79 @@
+class Solution {
+public:
+    int maxSumTwoNoOverlap(vector<int>& nums, int firstLen, int secondLen) {
+        int n = nums.size();
+       
+        // if(n==3){
+        //     int sum=0;
+        //     for(int i=0;i<n;i++){
+        //         sum += nums[i];
+        //     }
+        //     return sum;
+        // }
+        
+        int dp1[n];
+        int dp2[n];
+        int sum = 0;
+        
+        
+        for(int i = 0; i < n; i++){
+            if(i < firstLen){
+                sum += nums[i];
+                dp1[i] = sum;
+            } else {
+                sum += nums[i] - nums[i-firstLen];
+                dp1[i] = max(dp1[i-1],sum);
+            }
+        }
+        
+        sum = 0;
+        for(int i = n-1; i >= 0; i--){
+            if(i+secondLen >= n){
+                sum += nums[i];
+                dp2[i] = sum;
+            } else {
+                sum += nums[i] - nums[i+secondLen];
+                dp2[i] = max(dp2[i+1], sum);
+            }
+        }
+        
+        int ans = 0;
+        
+        for(int i = firstLen-1; i < n - secondLen; i++){
+            ans = max(ans, dp1[i]+dp2[i+1]);
+        }
+        
+//         from y to x
+        sum = 0;
+        
+        for(int i = 0; i < n; i++){
+            if(i < secondLen){
+                sum += nums[i];
+                dp1[i] = sum;
+            } else {
+                sum += nums[i] - nums[i-secondLen];
+                dp1[i] = max(dp1[i-1],sum);
+            }
+        }
+        
+        sum = 0;
+        for(int i = n-1; i >= 0; i--){
+            if(i+firstLen >= n){
+                sum += nums[i];
+                dp2[i] = sum;
+            } else {
+                sum += nums[i] - nums[i+firstLen];
+                dp2[i] = max(dp2[i+1], sum);
+            }
+        }
+        
+        // ans = 0;
+        for(int i = secondLen-1; i < n - firstLen; i++){
+            ans = max(ans, dp1[i]+dp2[i+1]);
+        }
+        
+        
+        return ans;
+        
+    }
+};
