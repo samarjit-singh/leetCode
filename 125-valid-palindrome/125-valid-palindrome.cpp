450c60c04781b6ba911e5ec8eda3dc3094138e5c
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-06-11 15:58:33 +0530

    Time: 10 ms (36.51%), Space: 7.3 MB (75.19%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.length()<=1) {
+            return true;
+        }
+        
+        int start =0 , end = s.length()-1;
+        
+        while(start < end){
+            while(start < end && !isalnum(s[start])){
+                start++;
+            }
+            while(start < end && !isalnum(s[end])){
+                end--;
+            }
+            if(start<end && tolower(s[start]) != tolower(s[end]))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
