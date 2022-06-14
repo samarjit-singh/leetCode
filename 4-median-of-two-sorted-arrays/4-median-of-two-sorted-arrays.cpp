9a5a4a9dd82e5a78575940a01c13ce3694a414d6
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-06-14 19:47:52 +0530

    Time: 51 ms (63.11%), Space: 89.3 MB (62.72%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<float> ans;
-        for(int i=0;i<nums1.size();i++){
-            ans.push_back(nums1[i]);
-        }
-        for(int i=0;i<nums2.size();i++){
-            ans.push_back(nums2[i]);
+//         
+        
+        if(nums2.size()<nums1.size()){   //do binary search for elemnts with smaller size
+            return findMedianSortedArrays(nums2,nums1);
         }
         
-        sort(ans.begin(),ans.end());
-        int n = ans.size();
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+        int low = 0 , high = n1;  //min you can choose 0 elements and max n1 elements
         
-        if(n%2!=0){
-            return ans[n/2];
-        } else {
-            return (ans[(n/2)-1]+ans[(n/2)])/2;
+        while(low <= high){
+            int cut1 = (low+high)/2;
+            int cut2 = (n1+n2+1)/2 - cut1;
+            
+            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+            
+            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+            
+            if(left1 <= right2 && left2 <= right1){
+                if( (n1+n2) % 2 == 0 )
+                    return (max(left1,left2) + min(right1,right2)) / 2.0;
+                else 
+                    return max(left1,left2);
+            } else if(left1>right2){
+                high = cut1 - 1;
+            } else {
+                low = cut1 + 1;
+            }
+            
         }
         
-        
+        return 0.0;
     }
 };
