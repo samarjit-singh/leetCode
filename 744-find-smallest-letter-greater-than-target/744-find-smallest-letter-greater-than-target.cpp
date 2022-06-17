b11b333fed6807889c831a4e2929f799f6ed39bb
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-06-17 12:41:05 +0530

    Time: 15 ms (89.15%), Space: 15.9 MB (69.50%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -14,13 +14,5 @@ public:
             }
         }
         return letters[start];
-        // if (letters.back() <= target) return letters.front();
-        // int low = 0, high = letters.size() - 1;
-        // while (low < high) {
-        //     auto mid = (low + high) / 2;
-        //     if (target < letters[mid]) high = mid;
-        //     else low = mid + 1;
-        // }
-        // return letters[low];
     }
 };
