dc0d6f73aea6a958da4621757a4d9559f6cef4ff
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-06-17 12:40:51 +0530

    Time: 26 ms (37.59%), Space: 16 MB (24.36%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        // if (letters.back() <= target) return letters.front();
-        // int n = letters.size();
-        // int start = 0;
-        // int end = n-1;
-        // while(start<=end){
-        //     auto mid = (start + end)/2;
-        //     if(target < letters[mid]){
-        //         end = mid;
-        //     } else {
-        //         start = mid+1;
-        //     }
-        // }
-        // return letters[start];
-        if (letters.back() <= target) return letters.front();
-        int low = 0, high = letters.size() - 1;
-        while (low < high) {
-            auto mid = (low + high) / 2;
-            if (target < letters[mid]) high = mid;
-            else low = mid + 1;
+        if (letters.back() <= target) 
+            return letters.front();
+        int start = 0;
+        int end = letters.size()-1;
+        while(start < end){
+            auto mid = (start + end) / 2;
+            if(target < letters[mid]){
+                end = mid;
+            } else {
+                start = mid+1;
+            }
         }
-        return letters[low];
+        return letters[start];
+        // if (letters.back() <= target) return letters.front();
+        // int low = 0, high = letters.size() - 1;
+        // while (low < high) {
+        //     auto mid = (low + high) / 2;
+        //     if (target < letters[mid]) high = mid;
+        //     else low = mid + 1;
+        // }
+        // return letters[low];
     }
 };
