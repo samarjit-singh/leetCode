e034562206fddf5b9ac4274b70053cda3cfe8a3b
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-07-23 17:40:10 +0530

    Time: 33 ms (15.23%), Space: 8.6 MB (83.19%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -2,11 +2,16 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
         
-        int n = num.size();
-        stack<char> st;
+        int n = num.size(); // take the size of string
+        stack<char> st;  // declare a stack
         
-        for(char c:num){
-            while(!st.empty() && k>0 && st.top()>c){ //we found the dip 
+        
+//         lets take eg 1432219 and k = 3
+        
+        for(char c:num){ // run the for loop
+//             if stack is not empty and k value is greater than 0 
+//             and if stack top is greater than element in the string
+            while(!st.empty() && k>0 && st.top()>c){ 
                 st.pop();
                 k -= 1;
             }
