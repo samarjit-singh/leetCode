999e957e8631e456321850625fc49f787e7176f8
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-08-04 15:23:58 +0530

    Time: 165 ms (99.12%), Space: 78 MB (90.03%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        	int n = nums.size();
-
-	deque<pair<int, int>> q; // <index, max_path_sum>
-	q.push_back({0, nums[0]});
-
-	for (int i = 1; i < n; ++i) {
-		int cur = nums[i] + q.front().second;
-		while (!q.empty() && cur >= q.back().second)
-			q.pop_back();
-		q.push_back({i, cur});
-		
-		if (q.front().first <= i - k)
-			q.pop_front();
-	}
-
-	return q.back().second;
+        
+        int n = nums.size();
+        
+        deque<pair<int,int>> dque;
+        dque.push_back({0, nums[0]});
+        
+        for(int i=1;i<n;++i){
+            int curr = nums[i] + dque.front().second;
+            while(!dque.empty() && curr >= dque.back().second){
+                dque.pop_back();
+            }
+            
+            dque.push_back({i,curr});
+            
+            if(dque.front().first <= i-k){
+                dque.pop_front();
+            }
+        }
+        return dque.back().second;
     }
 };
