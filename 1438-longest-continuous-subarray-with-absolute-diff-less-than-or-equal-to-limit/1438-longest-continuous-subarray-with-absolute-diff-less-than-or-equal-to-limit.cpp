3e95569179877092aa3a09556ba38f6c0a8624f0
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-08-04 12:25:49 +0530

    Time: 142 ms (88.03%), Space: 52.3 MB (58.22%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums, int limit) {
-        // maxElements will hold the elements in decreasing order, and
-        // minElements will hold the elements in increasing order
-        deque<int> maxElements, minElements;
         
-        // taking two pointers
-        // j will always move forward always, and
-        // i will only move forward, when max & min element will cross the limit
-        int i = 0, j;
-        for(j = 0 ; j < nums.size(); j++){
-            
-            // if maxElements is not empty & if its back is having smaller element than jth current element in nums, then pop it, bcz maxElements is maintaining a decreasing order
-            while(!maxElements.empty() && maxElements.back() < nums[j]){
-                maxElements.pop_back();
+        deque<int> maxElement,minElement;
+        
+        int i=0,j;
+        for(j=0;j<nums.size();j++){
+            while(!maxElement.empty() && maxElement.back()<nums[j]){
+                maxElement.pop_back();
             }
             
-            // if minElements is not empty & if its back is having greater element than jth current element in nums, then pop it, bcz itminElements is maintaining a increasing order
-            while(!minElements.empty() && minElements.back() > nums[j]){
-                minElements.pop_back();
+            while(!minElement.empty() && minElement.back()>nums[j]){
+                minElement.pop_back();
             }
             
-            // push the current jth value of nums, into maxElements & minElements
-            maxElements.push_back(nums[j]);
-            minElements.push_back(nums[j]);
+            maxElement.push_back(nums[j]);
+            minElement.push_back(nums[j]);
             
-            // if the max & min element will cross the limit, then i will move forward,
-            if(maxElements.front() - minElements.front() > limit){
-                
-                // if maxElements front is having same value as nums[i], it means needs to pop it, its a old value, and its out of the subarray
-                if(maxElements.front() == nums[i]) maxElements.pop_front();
-                
-                // if minElements front is having same value as nums[i], it means needs to pop it, its a old value, and its out of the subarray
-                if(minElements.front() == nums[i]) minElements.pop_front();
+            if(maxElement.front()-minElement.front() > limit){
+                if(maxElement.front() == nums[i])
+                    maxElement.pop_front();
+                if(minElement.front() == nums[i])
+                    minElement.pop_front();
                 i++;
             }
-        }        
-        
-        // return result
+            
+        }
         return j-i;
     }
 };
