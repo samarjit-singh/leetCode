dc1048161a5f48838fdf03d5e1a77218e843b291
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-08-14 13:45:44 +0530

    Time: 4 ms (50.82%), Space: 8.2 MB (54.54%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -14,14 +14,17 @@ public:
     
     int sum = 0;
     void dfs(TreeNode* root){
-        if(root==NULL){
+        
+        if(root==NULL){ // if root is = NULL
             return;
         }
-        dfs(root->right);
-        root->val += sum;
-        sum = root->val;
-        dfs(root->left);
-        return;
+        
+        dfs(root->right);// go to right
+        
+        root->val += sum; // add sum in root val
+        sum = root->val;//update sum with root value
+        dfs(root->left); // go to left
+        // return;
     }
     
     TreeNode* bstToGst(TreeNode* root) {
