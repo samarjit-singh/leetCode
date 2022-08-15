ef2573a2a0a0ca0a16eb834c40ccf517da209ce4
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-08-15 12:31:18 +0530

    Time: 33 ms (45.47%), Space: 16.7 MB (87.37%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.cpp
@@ -23,30 +23,19 @@ public:
             return NULL;
         }
         
-//         making current point to root node
         Node* current = root;
         
-//         till there are no nodes
-//         we are doing it for each level
         while(current->left){
-            
-//             making another pointer temp and making it point to current
-//             beacuse we need current for moving forward
             Node* temp = current;
             
             while(current){
-//                 for test case 1
-//                 suppose current is at root node
                 current->left->next = current->right;
                 current->right->next = current->next == NULL ? NULL : current->next->left;
                 current = current->next;
             }
-//             moving to next level
-//             we cannot do current->left because it is NULL
-            current = temp->left;
             
+            current = temp->left;
         }
-        
         return root;
     }
 };
