684a55d8ea0abdf9abfd321d9dfd767f280846a4
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-09-22 21:27:27 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (10.58%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int ans = 0;
+        reverse(s.begin(),s.end());
+        
+        int space = false;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' ' && space){
+                break;
+            }
+            
+            if(s[i]==' ' && !space ){ //if first character is the space
+                
+            } else {
+                ans++;
+                space = true;
+            }
+        }
+        return ans;
+    }
+};
