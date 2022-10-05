5ca1b57077c89cdbda403b4ec6f1b1d28f2ddd62
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-10-05 16:28:48 +0530

    Time: 194 ms (98.98%), Space: 68.4 MB (38.60%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        int n = 0;
+        int n = 0; // we need this variable for making a vector of size equal to the size of highest value node
         
         for(int i=0;i<edges.size();i++){
-            n = max(n,max(edges[i][0],edges[i][1]));
+            n = max(n,max(edges[i][0],edges[i][1])); // we do the above thing by finding out the maximum node from 2d array
         }
         
-        vector<int> indegree(n+1);
+        vector<int> indegree(n+1); // make a vector of size n
         
-        for(int i=0;i<edges.size();i++){
+        for(int i=0;i<edges.size();i++){  // we increase the value every time the node is visited
             indegree[edges[i][0]]++;
             indegree[edges[i][1]]++;
         }
@@ -17,13 +17,13 @@ public:
         int result;
         
         for(int i=0;i<indegree.size();i++){
-            if(indegree[i]==n-1){
+            if(indegree[i]==n-1){ //if any value in vector indgree matches n-1 we store its index in result 
                 result = i;
                 break;
             }
         }
         
-        return result;
+        return result; // and return the result
         
     }
 };
