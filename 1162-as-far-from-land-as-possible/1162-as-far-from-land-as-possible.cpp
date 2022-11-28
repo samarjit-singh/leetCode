74a00ed673ccd47433202fde5d21d4861d0bc92a
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-11-28 22:53:28 +0530

    Time: 208 ms (24.06%), Space: 29 MB (20.91%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -6,27 +6,31 @@ public:
         
         int steps = 0;
         
+        
+//         int these two for loop if we encounter any 1
+//         we will push all the 4 adjacent cell index in queue
         for(auto i=0;i<grid.size();++i){
             for(auto j=0;j<grid[i].size();++j){
                 if(grid[i][j]==1)
                     q.push({i-1,j}),q.push({i+1,j}),q.push({i,j-1}),q.push({i,j+1});
             }
         }
-                
-                while(!q.empty()){
-                    ++steps;
-                    while(!q.empty()){
-                        int i = q.front().first,j = q.front().second;
-                        q.pop();
-                        if(i>=0 && i<grid.size() && j>=0 && j<grid[i].size() && grid[i][j]==0){
-                            grid[i][j] = steps;
-                            q1.push({i-1,j}),q1.push({i+1,j}),q1.push({i,j-1}),q1.push({i,j+1});
-                        }
-                        
-                    }
-                    swap(q1,q);
+               
+//         run this loop till q is not empty
+        while(!q.empty()){
+            steps++; //increament the step counter
+            while(!q.empty()){ //now again run this loop till q is not empty
+                int i = q.front().first,j = q.front().second; //take out the index
+                q.pop();
+//                 check if they are valid and euqlas to 0
+                if(i>=0 && i<grid.size() && j>=0 && j<grid[i].size() && grid[i][j]==0){
+                    grid[i][j] = steps; //update cell value with steps so that we don't encounter it again
+                    q1.push({i-1,j}),q1.push({i+1,j}),q1.push({i,j-1}),q1.push({i,j+1});
                 }
-        
+                        
+            }
+            swap(q1,q);
+        }
         
         return steps==1 ? -1:steps-1;
         
