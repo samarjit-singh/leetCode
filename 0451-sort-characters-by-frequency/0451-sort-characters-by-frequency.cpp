74013d60faf06f0533f3390788b738865f6bd5a3
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2022-12-03 16:42:54 +0530

    Time: 12 ms (90.96%), Space: 8.4 MB (78.73%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        
+        unordered_map<char,int> umap;
+        
+        for(int i=0;i<s.size();i++){
+            umap[s[i]]++;
+        }
+        
+        vector<pair<int,char>> vec;
+        
+        for(auto it:umap){
+            vec.push_back({it.second,it.first});
+        }
+        
+        sort(vec.begin(),vec.end());
+        
+        string ans;
+        for(auto it:vec)
+        {
+            while(it.first!=0)
+            {
+                ans.push_back(it.second);//inserted according to the frequency
+                it.first--;
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+};
