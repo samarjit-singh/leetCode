d3bf961224539277774aedddbb006373fb1a1775
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-01-06 23:45:18 +0530

    Time: 469 ms (18.48%), Space: 76.4 MB (76.68%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -5,14 +5,17 @@ public:
         
         int n = costs.size();
         
+        int ans = 0;
+        
         for(int i=0;i<costs.size();i++){
             if(coins>=costs[i]){
+                ans++;
                 coins -= costs[i];
             } else {
                 return i;
             }
         }
         
-        return n;
+        return ans;
     }
 };
