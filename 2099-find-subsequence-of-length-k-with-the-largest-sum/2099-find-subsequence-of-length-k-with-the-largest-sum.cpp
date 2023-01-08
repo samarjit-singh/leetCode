db29b965dc6add5455e1929914c90f7d6301ed6a
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-01-08 16:23:53 +0530

    Time: 13 ms (49.93%), Space: 9.6 MB (55.50%) - LeetHub

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
     vector<int> maxSubsequence(vector<int>& nums, int k) {
-        vector<int> ans;
-        vector<int> temp = nums;
-        unordered_map<int,int> umap;
-        
-        sort(nums.begin(),nums.end(),greater<int>());
-        
-        for(int i=0;i<k;i++)
-            umap[nums[i]]++;
-        
+        vector<int> ans; // make ans vector
+        vector<int> temp = nums; // make a copy vector temp
+        unordered_map<int,int> umap; // umap for storing the ocuurance
+         
+        sort(nums.begin(),nums.end(),greater<int>()); // sort array in decreasing
+         
+        for(int i=0;i<k;i++) 
+            umap[nums[i]]++; // count the occurance
+         
         for(auto it:temp){
-            if(umap[it]-- > 0)
+            if(umap[it] > 0){
                 ans.push_back(it);
+                umap[it]--;
+            }
+                
         }
         
         return ans;
         
     }
 };
+
+// time complexity nlog(k)
