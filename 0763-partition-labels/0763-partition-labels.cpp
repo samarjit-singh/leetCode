b4fe841d7ec119443a8a5fc2abe64fc16e3c344c
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-01-11 14:56:54 +0530

    Time: 2 ms (90.84%), Space: 6.4 MB (97.23%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.cpp
+++ 0763-partition-labels/0763-partition-labels.cpp
@@ -13,7 +13,7 @@ public:
         for(int i=0;i<n;i++){
             maxi = max(maxi,a[s[i]-'a']);
             if(i == maxi){
-                ans.push_back(i-last+1);
+                ans.push_back(maxi-last+1);
                 last = i+1;
             }
         }
