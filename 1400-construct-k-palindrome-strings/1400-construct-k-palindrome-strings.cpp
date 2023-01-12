0ed8407af1cce6015d5d7e89806b582a631c6582
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-01-12 14:10:24 +0530

    Time: 30 ms (79.80%), Space: 11.7 MB (96.93%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.cpp
@@ -2,30 +2,42 @@ class Solution {
 public:
     bool canConstruct(string s, int k) {
         
-        vector<int> a(26);
+        vector<int> a(26); // make a vector to store the occurance of each character
         
         for(int i=0;i<s.size();i++){
-            a[s[i]-'a']++;
+            a[s[i]-'a']++; // insert the count in vector
         }
         
+//         declare odd even counters
         int odd = 0;
         int even = 0;
+        
         for(int i=0;i<26;i++){
-            if(a[i]%2 != 0){
+            if(a[i]%2 != 0){ // now check for odd occurance of characters
                 odd++;
-            } else {
+            } else { // now check for even occurance of characters
                 even++;
             }
         }
         
-        if(odd>k)
-            return false;
+        cout<<odd<<endl;
+        cout<<even;
         
-        if(even==k)
-            return true;
-        
-        if(s.size()<k)
+        if(odd>k){
+            cout<<"odd>k";
             return false;
+        }
+            
+        if(even==k){
+            cout<<"even==k";
+           return true; 
+        }
+            
+        if(s.size()<k){
+            cout<<"s.size()<k";
+            return false;
+        }
+            
         
         return true;
     }
