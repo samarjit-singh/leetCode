1936cb1a27d5d639cd5771be048cffbf2e8a9de9
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-01-19 00:39:25 +0530

    Time: 6 ms (44.82%), Space: 7.6 MB (79.76%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
-        if(nums.size()<=1)
-            return {nums};
-        
-        vector<vector<int>> result;
-        
-        for(int i=0;i<nums.size();++i){
-            vector<int> v(nums.begin(),nums.end()); // make a vector similar to nums
-            v.erase(v.begin() + i); // delete fro ith postion
-            auto res = permute(v);
-            
-            for(int j=0;j<res.size();j++){
-                vector<int> _v = res[j];
-                _v.insert(_v.begin(),nums[i]);
-                result.push_back(_v);
-             }
-            
+    
+    void ok(int index,vector<int>& nums,vector<vector<int>>& ans){
+        if(index==nums.size()){
+            ans.push_back(nums);
+            return;
         }
-        return result;
+        
+        for(int i=index;i<nums.size();i++){
+            swap(nums[index],nums[i]);
+            ok(index+1,nums,ans);
+            swap(nums[index],nums[i]);
+        }
+        
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        ok(0,nums,ans);
+        return ans;
     }
 };
