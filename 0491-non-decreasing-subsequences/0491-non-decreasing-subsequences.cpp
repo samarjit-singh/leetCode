746a2825fd2462a3b232e77478bf9940a14a24f5
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-01-23 22:36:35 +0530

    Time: 86 ms (60.33%), Space: 25.5 MB (58.15%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+    vector<vector<int>> ans;
+    
+    void dfs(vector<int>& nums,vector<int>& temp,int index){
+        if(temp.size()>1) ans.push_back(temp);
+        
+        unordered_set<int> set;
+        for(int i=index;i<nums.size();i++){
+            if((temp.size()==0 || temp.back()<=nums[i]) && (set.find(nums[i]) == set.end())){
+                temp.push_back(nums[i]);
+                dfs(nums,temp,i+1);
+                temp.pop_back();
+                set.insert(nums[i]);
+            }
+        }
+    }
+    
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<int> temp;
+        dfs(nums,temp,0);
+        return ans;
+    }
+};
