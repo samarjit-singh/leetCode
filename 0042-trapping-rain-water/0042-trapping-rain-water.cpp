fdc4fd16096022460a6ee620c7095a6fed1e84d4
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-02-02 23:09:22 +0530

    Time: 15 ms (82.90%), Space: 20 MB (54.74%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        int lmax[n];
-        lmax[0] = height[0];
+        int lmax[n]; // store all the maximum element while comparing from left to right
+        lmax[0] = height[0];  // store the first element
         
-        for(int i=1;i<n;i++){
-            int temp = max(height[i],lmax[i-1]);
-            lmax[i] = temp;
+        for(int i=1;i<n;i++){ 
+            int temp = max(height[i],lmax[i-1]); // find the maximum ele b/w the element and the left element 
+            lmax[i] = temp; // store the max ele in lmax
         }
         
-        int rmax[n];
-        rmax[n-1] = height[n-1];
+        int rmax[n]; // store all the maximum element while comparing from right to left
+        rmax[n-1] = height[n-1]; // store the last element
         
         for(int i=n-2;i>=0;i--){
-            int temp = max(height[i],rmax[i+1]);
-            rmax[i] = temp;
+            int temp = max(height[i],rmax[i+1]); // find the max ele b/w the ele and the right ele 
+            rmax[i] = temp; // store that in rmax
         }
         
-        int water = 0;
+        int water = 0; // make a variable water
         
-        for(int i=1;i<n-1;i++){
-            water += min(lmax[i],rmax[i]) - height[i];
+        for(int i=1;i<n-1;i++){ // traverse the height array from second ele to the second last ele
+            water += min(lmax[i],rmax[i]) - height[i]; // fidnthe min ele from lmax and rmax and subtract thta from height and add it in water
         }
         
-        return water;
+        return water; // return water
         
     }
 };
