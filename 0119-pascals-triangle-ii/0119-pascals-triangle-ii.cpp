874cacf391fd213e9314ea841b3ad80985d1a00c
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-02-06 22:47:46 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (81.19%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> v(rowIndex+1,1);
+        for(int i=0;i<rowIndex;i++){
+            for(int j=i;j>0;j--){
+                v[j] += v[j-1];
+            }
+        }
+        return v;
+    }
+};
