458a6673fcea277c33a50f5bb66bc2a294711392
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-02-09 02:25:53 +0530

    Time: 413 ms (5.00%), Space: 163 MB (5.29%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-     int maxProfit = 0;
-        int minProfit =INT_MAX;
-        for(int i=0;i<prices.size();i++){
-            minProfit = min(prices[i],minProfit);
-            maxProfit = max(maxProfit,prices[i]-minProfit);
+    
+    int util(vector<int>& prices,int i,int k,bool buy,vector<vector<int>>& v){
+        if(i >= prices.size() || k<=0) return 0;
+        
+        if(v[i][buy]!=-1) return v[i][buy];
+        
+        if(buy) {
+            //-prices[i], because bought stock of prices[i], expend money
+            return v[i][buy] = max(-prices[i]+util(prices,i+1,k,!buy,v),util(prices,i+1,k,buy,v));
+        } else { //it's time to sell , now decrease k, we have done 1 transaction
+            //+prices[i], because we now gain (i.e) sell our stock at rate of prices[i]
+            return v[i][buy] = max(prices[i]+util(prices,i+1,k-1,!buy,v), util(prices,i+1,k,buy,v));
         }
-        return maxProfit;
+        
+    }
+    
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> v(n,vector<int>(2,-1));
+//         we are passing prices vector,index, transactions , buy/sell , vector
+        return util(prices,0,1,1,v);
     }
 };
