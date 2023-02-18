23b5a8da503f63c17f047fc0e6f91e0fcdf34807
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-02-18 19:13:10 +0530

    Time: 440 ms (21.66%), Space: 401.3 MB (5.07%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -20,44 +20,47 @@ public:
     }
     
     bool find(int index,vector<int> &st, vector<vector<int>> &dp,int l){
-        if(index == st.size()-1){
+        
+        if(index == st.size()-1){ //if index is == to the last pos return true
             return 1;
         }
         
-        if(dp[index][l]!=-1)
+        if(dp[index][l]!=-1) // basic dp procedure
             return dp[index][l];
         
-        bool r=0,s=0,t=0;
+        bool r=0,s=0,t=0; // now initialize threee values for k-1, k , k+1 jumps
         
-        if(l>1){ // we cannot do a jump of 0 length need to be >=1
+        if(l>0){ // we cannot do a jump of 0 length need to be >=1
             
-        }
-        
-        if(l>0){
             int a = search(st,st[index]+l-1);
+            
             if(a!=-1){
                 r=find(a,st,dp,l-1);
             }
+            
             int b = search(st,st[index]+l);
+            
             if(b!=-1){
                 s=find(b,st,dp,l);
             }
         }
         
         
-        int c = search(st,st[index]+l+1);
+//         search the index in arry using the binary search function
+        int c = search(st,st[index]+l+1);  // first this statement will be persformed
         
+//         if it is present in the stones array
         if(c!=-1){
-            t=find(c,st,dp,l+1);
+            t=find(c,st,dp,l+1); // we recursively call the function
         }
         
+//         and if we get 1 from any of the 3 jumps we return 1
         return dp[index][l] = r||s||t;
         
     }
     
     bool canCross(vector<int>& stones) {
-        vector<vector<int>> dp(2001,vector<int>(2001,-1));
-        
+        vector<vector<int>> dp(2001,vector<int>(2001,-1)); // make a dp vector for stroing the states    
         return find(0,stones,dp,0);
     }
 };
