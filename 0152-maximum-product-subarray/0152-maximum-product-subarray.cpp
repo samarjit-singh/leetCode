4f20a224e0b8003f3352209a1552e9c8f4b62eb6
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-02-24 09:48:01 +0530

    Time: 3 ms (97.58%), Space: 13.9 MB (45.49%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int max_overall = nums[0];
-        int max_ending_here = nums[0];
-        int min_ending_here = nums[0];
         
-        for(int i = 1; i < nums.size(); ++i){
-            int temp = max_ending_here;
-            // max_ending_here = max({nums[i], nums[i]*temp, nums[i]*min_ending_here});
-            // min_ending_here = min({nums[i], nums[i]*temp, nums[i]*min_ending_here});
-            max_ending_here = max({nums[i],nums[i]*temp, nums[i]*min_ending_here});
-            min_ending_here = min({nums[i],nums[i]*temp, nums[i]*min_ending_here});
-            max_overall = max(max_overall, max_ending_here);
+        if(nums.size()==0){
+            return -1;
         }
-        return max_overall;
+        
+        int minProd = nums[0];
+        int maxProd = nums[0];
+        int ans = nums[0];
+        
+        int choice1, choice2;
+        
+        for(int i=1;i<nums.size();i++){
+            choice1 = nums[i]*minProd;
+            choice2 = nums[i]*maxProd;
+            maxProd = max(nums[i],max(choice1,choice2));
+            minProd = min(nums[i],min(choice1,choice2));
+            
+            ans = max(ans,maxProd);
+        }
+        
+        return ans;
+        
     }
 };
