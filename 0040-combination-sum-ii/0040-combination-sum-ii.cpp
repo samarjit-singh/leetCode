7e28b0d3ea65e6cf56add9994fdd03f13cc4e88a
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-04-03 20:15:11 +0530

    Time: 4 ms (73.66%), Space: 11.4 MB (25.13%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     
-    void ok(vector<int>& candidates,int target,int currIndex,vector<vector<int>>& ans,vector<int>& currComb){
-        // if(currSum>target)
-        //     return;
+    void ok(vector<int>& candidates, int target,int index,vector<int> currComb,vector<vector<int>>& ans){
         
-        if(target==0){
+        if(target == 0){
             ans.push_back(currComb);
             return;
         }
         
-        for(int i=currIndex;i<candidates.size();i++){
-            if(candidates[i] > target) break;
-      
+        for(int i=index;i<candidates.size();i++){
             
-            if(i  > currIndex && candidates[i] == candidates[i-1])
+            if(candidates[i]>target) break;
+            
+            if(i  > index && candidates[i] == candidates[i-1])
                 continue;
             
             currComb.push_back(candidates[i]);
-            ok(candidates,target-candidates[i],i+1,ans,currComb);
+            ok(candidates,target-candidates[i],i+1,currComb,ans);
             currComb.pop_back();
-            // currSum -=  candidates[i];
         }
+        
     }
     
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> curr;
         vector<vector<int>> ans;
-        vector<int> currComb;
         sort(candidates.begin(),candidates.end());
-        ok(candidates,target,0,ans,currComb);
+        ok(candidates,target,0,curr,ans);
         return ans;
-     }
+    }
 };
