19e3f6908a3cad985dea9fb8ced798f0f83a5b00
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-04-12 22:29:22 +0530

    Time: 11 ms (55.24%), Space: 14 MB (53.84%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,50 +1,46 @@
 class Solution {
 public:
     
-    bool isValid(vector<vector<int>>& image,int i,int j,int n,int m,int oldColor){
+    bool isValid(vector<vector<int>>& image,int i,int j,int n, int m,int oldColor){
         if(i>=0 && i<n && j>=0 && j<m && image[i][j] == oldColor){
             return true;
         }
         return false;
     }
     
-    void floodFillRec(vector<vector<int>>& image,int i,int j,int n,int m,int oldColor,int color){
+    void floodFill(vector<vector<int>>& image,int i,int j,int n,int m,int oldColor,int color){
+        
         image[i][j] = color;
         
         if(isValid(image,i+1,j,n,m,oldColor)){
-            floodFillRec(image,i+1,j,n,m,oldColor,color);
+            floodFill(image,i+1,j,n,m,oldColor,color);
         }
         
         if(isValid(image,i-1,j,n,m,oldColor)){
-            floodFillRec(image,i-1,j,n,m,oldColor,color);
+            floodFill(image,i-1,j,n,m,oldColor,color);
         }
         
         if(isValid(image,i,j+1,n,m,oldColor)){
-            floodFillRec(image,i,j+1,n,m,oldColor,color);
+            floodFill(image,i,j+1,n,m,oldColor,color);
         }
         
         if(isValid(image,i,j-1,n,m,oldColor)){
-            floodFillRec(image,i,j-1,n,m,oldColor,color);
+            floodFill(image,i,j-1,n,m,oldColor,color);
         }
+        
     }
     
-    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        
         int n = image.size();
         int m = image[0].size();
         
         int oldColor = image[sr][sc];
         
-        if(oldColor==color)
+        if(oldColor == color) 
             return image;
-                
-        floodFillRec(image,sr,sc,n,m,oldColor,color);
-                
-         
+        
+        floodFill(image,sr,sc,n,m,oldColor,color);
         
         return image;
-        
-        
     }
-};
+};   
