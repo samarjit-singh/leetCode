cf140cca4f1ebcbbc17ef1e19afa71ed85b34288
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-05-10 00:16:55 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (91.37%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        if(matrix.size() == 0) return {};
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        vector<int> res;
+        
+        int directionFlag = 0;
+        int top = 0 , down = n-1, left = 0 , right = m-1;
+        
+        while(top<=down && left <= right){
+            if(directionFlag == 0){
+                for(int i=left;i<=right;i++){
+                    res.push_back(matrix[top][i]);
+                }
+                top++;
+            } else if(directionFlag == 1){
+                for(int i=top;i<=down;i++){
+                    res.push_back(matrix[i][right]);
+                }
+                right--;
+            } else if(directionFlag == 2){
+                for(int i=right;i>=left;i--){
+                    res.push_back(matrix[down][i]);
+                }
+                down--;
+            } else if(directionFlag == 3){
+                for(int i=down;i>=top;i--){
+                    res.push_back(matrix[i][left]);
+                }
+                left++;
+            } 
+            directionFlag = (directionFlag+1)%6;
+        }
+        
+        return res;
+    
+    }
+};
