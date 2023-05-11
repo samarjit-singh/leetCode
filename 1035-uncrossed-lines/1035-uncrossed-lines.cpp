4284b16c6a6c7d9cbe9302f0537dd0e4f05f938b
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-05-11 23:44:46 +0530

    Time: 14 ms (84.21%), Space: 12.9 MB (63.96%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    
-    int solve(int i,int j, vector<int>& nums1,vector<int>& nums2,vector<vector<int>>& dp){
-        if(i<=0 || j<=0){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        
-        
-        if(nums1[i-1]==nums2[j-1]){
-            dp[i][j] = 1 + solve(i-1,j-1,nums1,nums2,dp);
-        } else {
-            dp[i][j] = max(solve(i-1,j,nums1,nums2,dp),solve(i,j-1,nums1,nums2,dp));
-        }
-        
-        return dp[i][j];
-    }
-    
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
+        int m = nums1.size();
+        int n = nums2.size();
         
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
+    
         
-        return solve(n,m,nums1,nums2,dp);
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(nums1[i-1]==nums2[j-1]) 
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                else 
+                    dp[i][j] = max(dp[i][j-1],dp[i-1][j]);
+            } 
+        }
+        
+        return dp[m][n];
     }
 };
