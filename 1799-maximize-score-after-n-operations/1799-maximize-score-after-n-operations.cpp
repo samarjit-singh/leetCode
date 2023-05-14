d92b47e1e8509ab9e1b846b7ca8245d4bf451e21
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-05-14 14:18:06 +0530

    Time: 608 ms (11.08%), Space: 24.5 MB (18.34%) - LeetHub

---
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -0,0 +1,41 @@
+class Solution {
+    int solve(vector<int>& nums,string& visited,int k){
+        if(k==0){
+            return 0;
+        } 
+        
+        if(dp[visited] != 0){
+            return dp[visited];
+        }
+        
+        int ans = 0;
+        
+        for(int i=0;i<nums.size();i++){
+            if(visited[i] == '0'){
+                visited[i] = '1';
+                int temp = 0;
+                for(int j=i+1;j<nums.size();j++){
+                    if(visited[j] == '0'){
+                        visited[j] = '1';
+                        temp = max(temp, k * __gcd(nums[i],nums[j]) + solve(nums,visited,k-1));
+                        visited[j] = '0';
+                    }
+                }
+                visited[i] = '0';
+                ans = max(temp,ans);
+            }
+        }
+        
+       return dp[visited] = ans;
+    }
+public:
+    
+    unordered_map<string,int> dp;
+    
+    int maxScore(vector<int>& nums) {
+        int n = nums.size();
+        string visited(n,'0');
+        
+        return solve(nums,visited,n/2);
+    }
+};
