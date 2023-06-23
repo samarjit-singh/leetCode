c1b39868e37d2ccd41285960c6fb91216bc94619
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-06-23 23:43:22 +0530

    Time: 263 ms (30.02%), Space: 96.3 MB (5.67%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     
     int dfs(int i,int buy,int n,vector<int>& prices,vector<vector<int>>& dp,int fee){
-        
-        if(i==n) return 0; // nothing to buy or sell
+        if(n==i){
+            return 0; //nothing to sell or buy
+        }
         
         if(dp[i][buy]!=-1) return dp[i][buy];
         
-        if(buy){ // if we buy is 1 return max of if we buy that stock or sell that stock
-            return dp[i][buy] = max(-prices[i]+dfs(i+1,0,n,prices,dp,fee),dfs(i+1,1,n,prices,dp,fee));
+        if(buy){
+            return dp[i][buy] = max(-prices[i]+dfs(i+1,0,n,prices,dp,fee), dfs(i+1,1,n,prices,dp,fee));
         }
         
-        return dp[i][buy] = max(prices[i]-fee+dfs(i+1,1,n,prices,dp,fee),dfs(i+1,0,n,prices,dp,fee));
-        
+        return dp[i][buy] = max(prices[i]+dfs(i+1,1,n,prices,dp,fee)-fee, dfs(i+1,0,n,prices,dp,fee));
     }
     
     int maxProfit(vector<int>& prices, int fee) {
