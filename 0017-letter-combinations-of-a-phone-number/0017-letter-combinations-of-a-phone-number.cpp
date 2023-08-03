f3814125ddc49888d46aa02c22a65ec7dba44679
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-08-04 01:11:11 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (55.13%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     
-    void printkeypad(int num,string output,string options[],vector<string>& ans){
+    void util(int num,string output,string options[],vector<string>& ans){
         
-        if(num==0){
+        if(num == 0){
             ans.push_back(output);
             return;
         }
         
         int n1 = num%10;
         string s1 = options[n1];
-        for(int i=0;i<s1.length();i++){
-            printkeypad(num/10,s1[i]+output,options,ans);
+        for(int i=0;i<s1.size();i++){
+            util(num/10,s1[i]+output,options,ans);
         }
+        
+        
+        
     }
     
     vector<string> letterCombinations(string digits) {
-        
         vector<string> ans1;
+        
         if(digits==""){
             return ans1;
         }
         
-        vector<string> ans;
-        
         int num = stoi(digits);
+        
         string output = "";
         string options[] = {"", "", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        printkeypad(num,output,options,ans);
         
-        return ans;
+        util(num,output,options,ans1);
+        
+        return ans1;
         
     }
 };
