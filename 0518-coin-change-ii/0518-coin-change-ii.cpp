bfc0c303c35fa47190798ce5a5a151170af87fcb
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-08-11 18:54:12 +0530

    Time: 35 ms (47.26%), Space: 18.6 MB (29.68%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        vector<vector<int>> dp(coins.size() + 1, vector<int> (amount+1,0));
+        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,0));
         
-        for(int i=0;i<=coins.size();i++){ // rows denotes the coins
-            for(int j=0;j<=amount;j++){ // columns denote the amount
-                if(j == 0) dp[i][j] = 1; // if amount 0 we can take only one coin
-                else if(i==0) dp[i][j] = 0; // if coins are 0 , 0 combinations will be made
-                else // we take the value from one cell above + amount -  coins[i-1]
-                    dp[i][j] = dp[i-1][j] + (j-coins[i-1] < 0 ? 0 : dp[i][j-coins[i-1]] );
+        for(int i=0;i<=coins.size();i++){
+            for(int j=0;j<=amount;j++){
+                if(j==0){ // if amount is zero only one combination can be made
+                    dp[i][j] = 1;
+                } else if(i==0){ // if zero coins are there no combination can be made
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i-1][j] + (j-coins[i-1]<0 ? 0 : dp[i][j-coins[i-1]] );
+                }
             }
         }
-        
         return dp[coins.size()][amount];
     }
 };
