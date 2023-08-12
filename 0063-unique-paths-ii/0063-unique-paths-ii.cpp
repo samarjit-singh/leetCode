2afd35d5010493e90bb74ca5679693c86104cfa2
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-08-12 18:21:08 +0530

    Time: 2 ms (68.17%), Space: 7.9 MB (25.52%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size(); // take the size of rows
-        int n = obstacleGrid[0].size(); // and column
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
         
-        vector<vector<int>> dp(m, vector<int> (n,0)); // make a 2D vector
-         
-        for(int i=0;i<m;i++){
-            if( obstacleGrid[i][0] == 1 ) break;
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        
+        for(int i=0;i<n;i++){
+            if(obstacleGrid[i][0] == 1) break;
             dp[i][0] = 1;
         }
         
-        for(int i=0;i<n;i++){
+        for(int i=0;i<m;i++){
             if(obstacleGrid[0][i] == 1) break;
             dp[0][i] = 1;
         }
         
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j] = (obstacleGrid[i][j] == 1) ? 0 : dp[i-1][j]+dp[i][j-1];
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
             }
         }
         
-        return dp[m-1][n-1];
+        return dp[n-1][m-1];
     }
 };
-
-// apraoch is simple
