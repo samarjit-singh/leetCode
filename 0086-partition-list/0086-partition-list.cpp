092264312d27b968e81a8d9941bfb51d586847ca
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-08-15 23:44:30 +0530

    Time: 4 ms (78.23%), Space: 10.2 MB (83.57%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -11,27 +11,26 @@
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        // make two listNode left and right
-        ListNode* left = new ListNode(-1); 
+        ListNode* left = new ListNode(-1);
         ListNode* right = new ListNode(-1);
-//         make another two tail node pointing to left and right
+        
         ListNode* ltail = left;
         ListNode* rtail = right;
         
         while(head!=NULL){
-            if(head->val < x){ // if head val is smaller than x 
-                ltail->next = head; // make ltail next point to head 
-                ltail = ltail->next; // move ltail forward
-            } else { // if not then right
-                rtail->next = head; //make rtail next point to head
-                rtail = rtail->next; //move rtail forward
+            if(head->val<x){
+                ltail->next = head;
+                ltail = ltail->next;
+            } else {
+                rtail->next = head;
+                rtail = rtail->next;
             }
-            head = head->next; //head will move forward to iterate over the input list everytime
+            head = head->next;
         }
         
-//         now ltail next is pointing to NULL make it poiny to right
-        ltail->next = right->next; // right is -1 so point it to right next
+        ltail->next = right->next;
         rtail->next = NULL;
+        
         return left->next;
     }
 };
