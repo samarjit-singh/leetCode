0d060d8275a59f51ddd50928acdacc7316cb2ad7
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-10-11 00:59:52 +0530

    Time: 282 ms (40.17%), Space: 64.6 MB (69.67%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -10,15 +10,20 @@ public:
                 nums[uniqueLen++] = nums[i];
             }
         }
-
+        
+        for(int i=0;i<nums.size();i++){
+            cout<<nums[i]<<" ";    
+        }
+        cout<<endl;
+        cout<<uniqueLen;
+        
         int ans = nums.size();
 
-        for (int i = 0, j = 0; j < uniqueLen;) {
+        for (int i = 0, j = 0; j < uniqueLen;i++) {
             while (j < uniqueLen && nums[j] - nums[i] <= nums.size() - 1) {
                 j++;
             }
             ans = min(ans, int(nums.size()) - (j - i));
-            i++;
         }
 
         return ans;
