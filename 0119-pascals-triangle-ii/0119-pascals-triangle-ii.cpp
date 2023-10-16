281950271910f80b876e057da7c48e1d2f250b26
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-10-16 17:13:12 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (51.35%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<int> v(rowIndex+1,1);
+        
         for(int i=0;i<rowIndex;i++){
             for(int j=i;j>0;j--){
                 v[j] += v[j-1];
