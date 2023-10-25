af8994f050be791604ababae653a36bf72a74294
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-10-25 23:56:36 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (28.86%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int kthGrammar(int n, int k) {
+        bool areValuesSame = true; 
+
+        // Calculate the total number of elements in the nth row, which is 2^(n-1).
+        n = pow(2, n - 1);
+
+        // Continue until we reach the first row.
+        while (n != 1) {
+            // Halve the number of elements in the row.
+            n /= 2;
+
+            // If k is in the second half of the row, adjust k and toggle the flag.
+            if (k > n) {
+                k -= n;
+                areValuesSame = !areValuesSame;
+            }
+        }
+
+        // Return 0 if the flag indicates that the values are the same; otherwise, return 1.
+        return ((areValuesSame) ? 0 : 1) ;
+    }
+};
