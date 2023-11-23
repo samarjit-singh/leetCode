66f33de775446c8cbef8c3141b49cadc44b2835b
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2023-11-24 02:03:22 +0530

    Time: 56 ms (76.26%), Space: 13.6 MB (74.23%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,37 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        vector<vector<int>> res;
         int n = nums.size();
+        vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
-        set<vector<int>> store;
-        long targets = target;
-        
         for(int i=0;i<n;i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
             for(int j=i+1;j<n;j++){
-                
-                long target_2 = targets - nums[i]-nums[j];
-                
-                int front = j+1;
-                int back = n-1;
-                
-                while(front<back){
-                    long two_sum = nums[front]+nums[back];
-                    
-                    if(two_sum<target_2) front++;
-                    else if(two_sum>target_2) back--;
-                    else {
-                       store.insert({nums[i],nums[j],nums[front],nums[back]});
-                        front++;
-                        back--;
-                    };   
+                if(j!=(i+1) && nums[j] == nums[j-1]) continue;
+                int k = j+1;
+                int l = n-1;
+                while(k<l){
+                    long long sum = nums[i];
+                    sum += nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+                    if(sum == target){
+                        vector<int> temp = {nums[i],nums[j],nums[k],nums[l]};
+                        ans.push_back(temp);
+                        k++; l--;
+                        while(k<l && nums[k] == nums[k-1]) k++;
+                        while(k<l && nums[l] == nums[l+1]) l--;
+                    } else if(sum<target) k++;
+                    else l--;
                 }
             }
         }
-        
-        for(auto i : store){
-            res.push_back(i);  // store the answers in an array(ans)
-        }
-        return res;
+        return ans;
     }
 };
