ec0c5d8b564246a48459364444a0b084f1bebe3f
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2024-12-22 16:55:30 +0530

    Time: 23 ms (86.16%), Space: 23 MB (32.11%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,47 +1,51 @@
 class Solution {
 public:
-    
-    bool isValid(int i,int j,int n,int m,vector<vector<char>>& grid){
-        if(i>=0 && i<n && j>=0 && j<m && grid[i][j] == '1'){
-            return true;
-        }
-        return false;
-    }
-    
-    void numIslandsRec(int i,int j,int n,int m,vector<vector<char>>& grid){
-        grid[i][j] = '0';
-        
-        if(isValid(i+1,j,n,m,grid))
-            numIslandsRec(i+1,j,n,m,grid);
-        
-        if(isValid(i-1,j,n,m,grid))
-            numIslandsRec(i-1,j,n,m,grid);
-        
-        if(isValid(i,j+1,n,m,grid))
-            numIslandsRec(i,j+1,n,m,grid);
-        
-        if(isValid(i,j-1,n,m,grid))
-            numIslandsRec(i,j-1,n,m,grid);
-            
-    }
-    
-    
-    int numIslands(vector<vector<char>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        
-        int ans = 0;
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                    ans++;
-                    numIslandsRec(i,j,n,m,grid);
+
+    const vector<pair<int,int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+    void bfs(vector<vector<char>>& grid, int startRow,int startCol){
+        int rows = grid.size();
+        int cols = grid[0].size();
+        queue<pair<int,int>> bfsQueue;
+
+        bfsQueue.push({startRow,startCol});
+        grid[startRow][startCol] = '0';
+
+        while(!bfsQueue.empty()){
+            auto [currentRow, currentCol] = bfsQueue.front();
+            bfsQueue.pop();
+
+            for(int i=0;i<4;i++){
+                int neighborRow = currentRow + directions[i].first;
+                int neighborCol = currentCol + directions[i].second;
+
+                if(neighborRow >= 0 && neighborRow < rows && neighborCol>=0 
+                    && neighborCol< cols && grid[neighborRow][neighborCol] == '1') {
+                        grid[neighborRow][neighborCol] = '0';
+                        bfsQueue.push({neighborRow,neighborCol});
                 }
-                
             }
         }
-        
-        return ans;
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        if(grid.empty()){
+            return 0;
+        }
+
+        int landCount = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+
+        for(int row=0;row<rows;row++){
+            for(int col=0;col<cols;col++){
+                if(grid[row][col] == '1'){
+                    landCount++;
+                    bfs(grid,row,col);
+                }
+            }
+        }
+
+        return landCount;
     }
 };
