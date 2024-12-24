8a70340cb012a12568b78fa305ccc3b5b3becc45
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2024-12-24 13:30:50 +0530

    Time: 43 ms (88.5%), Space: 29.1 MB (46.47%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> umap;
-        vector<int> result;
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (umap.find(target - nums[i]) != umap.end()) {
-                result = {nums[umap[target - nums[i]]], nums[i]};
-                break;
-            } else {
-                umap[nums[i]] = i;
-            }
-        }
-
-        return result;
-    }
-
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> res;
+        sort(nums.begin(),nums.end());
 
-        sort(nums.begin(), nums.end()); // Ensure the array is sorted to handle duplicates.
+        for(int i=0;i<nums.size();i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates for the first element.
+            int left = i+1;
+            int right = nums.size()-1;
 
-            int target = -nums[i];
-            unordered_map<int, int> seen;
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (seen.find(target - nums[j]) != seen.end()) {
-                    res.push_back({nums[i], target - nums[j], nums[j]});
-                    // Skip duplicates for the second and third elements.
-                    while (j + 1 < nums.size() && nums[j] == nums[j + 1]) j++;
+            while(left < right) {
+                int currentSum = nums[i] + nums[left] + nums[right];
+
+                if(currentSum > 0){
+                    right--;
+                } else if (currentSum < 0){
+                    left++;
+                } else {
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                while(left < right && nums[left] == nums[left-1]){
+                    left++;
                 }
-                seen[nums[j]] = j;
+                }
+
+
             }
+
         }
 
         return res;
