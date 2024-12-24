4eec39ff6aea8b73dd1d1dfcfa18d164899fa44f
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2024-12-24 13:10:11 +0530

    Time: 2098 ms (5.01%), Space: 471.9 MB (5%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,31 +1,42 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        if(n<3) return {};
-        
-        vector<vector<int>> result;
-        sort(nums.begin(),nums.end());
-        
-        for(int i=0;i<n-2;i++){
-            if(i==0 || nums[i]!=nums[i-1]){
-                int j = i+1, k = n-1;
-                while(j<k){
-                    int sum = nums[i] + nums[j] + nums[k];
-                    if(sum == 0){
-                        result.push_back({nums[i],nums[j],nums[k]});
-                        while(j<k && nums[j] == nums[j+1]) j++;
-                        while(j<k && nums[k] == nums[k-1]) k--;
-                        j++;
-                        k--;
-                    } else if (sum>0)
-                        k--;
-                    else 
-                        j++;
-                    
-                }
+
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> umap;
+        vector<int> result;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (umap.find(target - nums[i]) != umap.end()) {
+                result = {nums[umap[target - nums[i]]], nums[i]};
+                break;
+            } else {
+                umap[nums[i]] = i;
             }
         }
+
         return result;
+    }
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+
+        sort(nums.begin(), nums.end()); // Ensure the array is sorted to handle duplicates.
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // Skip duplicates for the first element.
+
+            int target = -nums[i];
+            unordered_map<int, int> seen;
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (seen.find(target - nums[j]) != seen.end()) {
+                    res.push_back({nums[i], target - nums[j], nums[j]});
+                    // Skip duplicates for the second and third elements.
+                    while (j + 1 < nums.size() && nums[j] == nums[j + 1]) j++;
+                }
+                seen[nums[j]] = j;
+            }
+        }
+
+        return res;
     }
 };
