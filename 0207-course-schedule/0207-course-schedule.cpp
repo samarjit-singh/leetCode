55410db7f038e1c4d2459e9a5fea70c72dc3a316
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-02-05 00:04:01 +0530

    Time: 8 ms (41.31%), Space: 22.4 MB (7.68%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,49 +1,42 @@
 class Solution {
 public:
-    
-    
-    bool DFSRec(int s,vector<bool>&visited,vector<bool>&currVisited,vector<int>adj[]){
-        visited[s] = true;
-        currVisited[s] = true;
-        
-        vector<int> data = adj[s];
-        
-        for(auto x : data){
-            if(visited[x]==false){
-                if(DFSRec(x,visited,currVisited,adj)){
-                    return true;
-                }
-            } else if(visited[x] && currVisited[x]){
-                return true;
-            }
-        }
-        
-        currVisited[s] = false;
-        return false;
-    }
-    
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = numCourses;
-        
-        vector<bool> visited(n,false), currVisisted(n,false);
-        
-        vector<int> adj[n]; //in this vector we will push all the adjacent elements of prerequisites[i] i.e prerequisites[i][j][1]
-        
-        for(auto x: prerequisites){
-            vector<int> data = x;
-            int a = data[0];
-            int b = data[1];
-            adj[a].push_back(b);
+        vector<vector<int>> adjList(numCourses);
+
+        for(auto prereq: prerequisites){
+            adjList[prereq[1]].push_back(prereq[0]);
         }
-        
-        for(int i=0;i<n;i++){
-            if(visited[i]==false){
-                if(DFSRec(i,visited,currVisisted,adj)){
-                    return false;
+
+        vector<bool> visited(numCourses, false);
+        vector<bool> inStack(numCourses, false);
+
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                stack<int> s;
+                s.push(i);
+
+                while(!s.empty()){
+                    int curr = s.top();
+
+                    if(!visited[curr]){
+                        visited[curr] = true;
+                        inStack[curr] = true;
+
+                        for(auto neighbor: adjList[curr]){
+                            if(!visited[neighbor]){
+                                s.push(neighbor);
+                            } else if(inStack[neighbor]){
+                                return false;
+                            }
+                        }
+                    } else {
+                        inStack[curr] = false;
+                        s.pop();
+                    }
                 }
             }
         }
-        
+
         return true;
     }
 };
