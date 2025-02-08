4d4b67ac2162532416c3ab746b1168f8a8a684c1
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-02-08 17:00:28 +0530

    Time: 24 ms (13.36%), Space: 108.1 MB (10.45%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,21 +1,41 @@
 class Solution {
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        int  peri=0;
-        int r = grid.size();
-        int c = grid[0].size();
-        
-        for(int i=0;i<r;i++){
-            for(int j =0;j<c;j++){
-                if(grid[i][j]==0) continue;
-                peri += 4;
-                if(i>0) peri -= grid[i-1][j];
-                if(j>0) peri -= grid[i][j-1];
-                if(i<r-1) peri -= grid[i+1][j];
-                if(j<c-1) peri -= grid[i][j+1];
+        int row = grid.size();
+        int col = grid[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<bool>> visited(row, vector<bool>(col, false));
+
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j] == 1){
+                    q.push({i,j});
+                    visited[i][j] = true;
+                    break;
+                }
             }
         }
-        
-        return peri;
+
+        int perimeter = 0;
+        vector<pair<int,int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+        while(!q.empty()){
+            auto [r,c] = q.front();
+            q.pop();
+
+            for(auto [dr,dc] : directions){
+                int nr = r + dr;
+                int nc = c + dc;
+
+                if(nr < 0 || nr >= row || nc < 0 || nc >= col || grid[nr][nc] == 0 ){
+                    perimeter++;
+                } else if(!visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    q.push({nr,nc});
+                }
+            }
+        }
+
+        return perimeter;
     }
 };
