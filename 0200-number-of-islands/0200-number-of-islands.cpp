00f547821c183c82c2b1c020b041977a747d4e45
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-02-11 10:25:31 +0530

    Time: 22 ms (88.49%), Space: 23.2 MB (34.51%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,51 +1,47 @@
 class Solution {
 public:
 
-    const vector<pair<int,int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+    vector<pair<int,int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
 
-    void bfs(vector<vector<char>>& grid, int startRow,int startCol){
+    void bfs(vector<vector<char>>& grid, int startRow, int startCol){
         int rows = grid.size();
         int cols = grid[0].size();
         queue<pair<int,int>> bfsQueue;
 
-        bfsQueue.push({startRow,startCol});
         grid[startRow][startCol] = '0';
+        bfsQueue.push({startRow, startCol});
 
         while(!bfsQueue.empty()){
-            auto [currentRow, currentCol] = bfsQueue.front();
+            auto [startRow, startCol] = bfsQueue.front();
             bfsQueue.pop();
 
             for(int i=0;i<4;i++){
-                int neighborRow = currentRow + directions[i].first;
-                int neighborCol = currentCol + directions[i].second;
+                int nr = startRow + directions[i].first;
+                int nc = startCol + directions[i].second;
 
-                if(neighborRow >= 0 && neighborRow < rows && neighborCol>=0 
-                    && neighborCol< cols && grid[neighborRow][neighborCol] == '1') {
-                        grid[neighborRow][neighborCol] = '0';
-                        bfsQueue.push({neighborRow,neighborCol});
+                if(nr>=0 && nr<rows && nc>=0 && nc<cols && grid[nr][nc] =='1'){
+                    grid[nr][nc] = '0';
+                    bfsQueue.push({nr,nc});
                 }
             }
         }
+
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        if(grid.empty()){
-            return 0;
-        }
-
-        int landCount = 0;
         int rows = grid.size();
         int cols = grid[0].size();
+        int island = 0;
 
         for(int row=0;row<rows;row++){
             for(int col=0;col<cols;col++){
                 if(grid[row][col] == '1'){
-                    landCount++;
-                    bfs(grid,row,col);
+                    bfs(grid, row, col);
+                    island++;
                 }
             }
         }
 
-        return landCount;
+        return island;
     }
 };
