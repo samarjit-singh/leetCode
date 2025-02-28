a2b6002e0aaede4ca1d017a670998623f5a2f5d0
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-02-28 12:17:03 +0530

    Time: 3 ms (42.08%), Space: 14.7 MB (21.9%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    
-    bool dfs(vector<vector<int>>& graph,vector<bool>& vis,int cur,int par = -1){
-        if(vis[cur]) return true; // node already visited - cycle exist
-        
-        vis[cur] = true; // make current as visited
-        
-        for(auto child : graph[cur]){ // check in the adjacency list for each node
-            
-//             if child is not its parent and there exist a cycle in the graph
-            if(child != par && dfs(graph,vis,child,cur) == true) return true;
-        }
-        
-        return false; // no cycle found
-        
-    }
-    
-    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        int n = edges.size(); // take the size of edges
-        vector<vector<int>> graph(n+1); // for storing the edges of each node
-        vector<bool> vis(n+1); // marking the visited nodes
-        
-        for(auto& E : edges){
-            fill(begin(vis), end(vis),false); // initialize visited with false
-            
-//             make a adjacency list
-             graph[E[0]].push_back(E[1]);
-             graph[E[1]].push_back(E[0]);
 
-            
-            if(dfs(graph,vis,E[0]) == true) // if cycle is detected  return the edge
-                return E;
-            
+    bool dfs(int node, int parent, vector<vector<int>>& adjList, vector<bool>& visit){
+        if(visit[node]) return true;
+
+        visit[node] = true;
+
+        for(int neighbour : adjList[node]){
+            if(neighbour == parent) continue;
+            if(dfs(neighbour, node, adjList, visit)) return true;
         }
-        
-        return { };
-        
+
+        return false;
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<vector<int>> adjList(n+1);
+
+        for(auto edge:edges){
+            adjList[edge[0]].push_back(edge[1]);
+            adjList[edge[1]].push_back(edge[0]);
+            vector<bool> visit(n+1, false);
+
+            if(dfs(edge[0], -1, adjList, visit)){
+                return {edge[0], edge[1]};
+            }
+        }
+
+        return {};
     }
 };
