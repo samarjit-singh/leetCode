cf4728f0d8a316ecfb5538e470e6eb2a7b8a2503
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-03-05 15:54:29 +0530

    Time: 3 ms (42.17%), Space: 13.9 MB (83.23%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
-    
-    void ok(vector<int>& candidates, int target,int index,vector<int> currComb,vector<vector<int>>& ans){
-        
-        if(target == 0){
-            ans.push_back(currComb);
+
+    int sumOfSubset(vector<int>& subset) {
+        int sum = 0;
+
+        for(int i=0;i<subset.size();i++){
+            sum += subset[i];
+        }
+
+        return sum;
+    }
+
+    void backtrack(int i, vector<int>& candidates, vector<vector<int>>& res, vector<int>& subset, int target) {
+        if(sumOfSubset(subset) == target){
+            res.push_back(subset);
             return;
         }
-        
-        for(int i=index;i<candidates.size();i++){
-            
-            if(candidates[i]>target) break;
-            
-//             never understood the meaning of this line
-            if(i  > index && candidates[i] == candidates[i-1]) continue;
-            
-            currComb.push_back(candidates[i]);
-            ok(candidates,target-candidates[i],i+1,currComb,ans);
-            currComb.pop_back();
+
+        if(i >= candidates.size() || sumOfSubset(subset) > target) {
+            return;
         }
-        
+
+        subset.push_back(candidates[i]);
+        backtrack(i+1, candidates, res, subset, target);
+
+        subset.pop_back();
+
+        while (i + 1 < candidates.size() && candidates[i] == candidates[i + 1]) {
+            i++;
+        }
+
+        backtrack(i+1, candidates, res, subset, target);
     }
-    
+
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<int> curr;
-        vector<vector<int>> ans;
-        sort(candidates.begin(),candidates.end());
-        ok(candidates,target,0,curr,ans);
-        return ans;
+       vector<vector<int>> res;
+       vector<int> subset;
+       sort(candidates.begin(),candidates.end());
+       backtrack(0, candidates, res, subset, target);
+
+       return res; 
     }
 };
