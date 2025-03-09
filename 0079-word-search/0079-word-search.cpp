6f54504744d5c8f1c6e8201124ea9b5a13cf7b7e
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-03-09 16:59:17 +0530

    Time: 787 ms (37.11%), Space: 11.5 MB (47.76%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,48 +1,39 @@
 class Solution {
 public:
-    
-    bool search(vector<vector<char>>& board, string& word,int i,int j,int idx,vector<vector<bool>>& visited){
-        
-        if(idx == word.length()-1){
+    vector<pair<int, int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+
+    bool dfs(vector<vector<char>>& board, string word, int index, int row, int col){
+        if(index == word.size()){
             return true;
         }
-        
-        visited[i][j] = true;
-        
-        if(i>0 && !visited[i-1][j] && board[i-1][j] == word[idx+1] && search(board,word,i-1,j,idx+1,visited)){
-            return true;
+
+        if(row<0 || row>=board.size() || col<0 || col>=board[0].size() || board[row][col]!=word[index]){
+            return false;
         }
-        
-        if(j>0 && !visited[i][j-1] && board[i][j-1] == word[idx+1] && search(board,word,i,j-1,idx+1,visited)){
-            return true;
+
+        char temp = board[row][col];
+        board[row][col] = '#';
+
+        for(auto [dr,dc]:directions){
+            if(dfs(board,word, index+1, row+dr, col+dc)){
+                return true;
+            }
         }
-        
-        if(i<board.size()-1 && !visited[i+1][j] && board[i+1][j] == word[idx+1] && search(board,word,i+1,j,idx+1,visited)){
-            return true;
-        }
-        
-        if(j<board[0].size() && !visited[i][j+1] && board[i][j+1] == word[idx+1] && search(board,word,i,j+1,idx+1,visited)){
-            return true;
-        }
-        
-        visited[i][j] = false;
-        
+
+        board[row][col] = temp;
         return false;
     }
-    
+
     bool exist(vector<vector<char>>& board, string word) {
-        int r = board.size();
-        int c = board[0].size();
-        
-        vector<vector<bool>> visited(r, vector<bool>(c,false));
-        
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(board[i][j] == word[0] && search(board,word,i,j,0,visited)){
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(dfs(board, word, 0, i, j)){
                     return true;
                 }
             }
         }
+
+
         return false;
     }
 };
