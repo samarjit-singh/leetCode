733e4c66d25f8b3985974d3438a2124d7b8ac7fa
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-03-10 18:02:10 +0530

    Time: 32 ms (80.8%), Space: 53.5 MB (82.63%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+
     bool isPalindrome(string s){
         int l = 0;
         int r = s.size()-1;
@@ -11,16 +11,18 @@ public:
         }
         return true;
     }
-    
-    void helper(string s,vector<vector<string>>& result,vector<string> temp){
-        if(s.size()==0)
+
+    void dfs(string s, vector<vector<string>>& result, vector<string>& temp){
+        if(s.size() == 0){
             result.push_back(temp);
-        
+            return;
+        }
+
         for(int i=0;i<s.size();i++){
-            string leftPar = s.substr(0,i+1);
-            if(isPalindrome(leftPar)){
-                temp.push_back(leftPar);
-                helper(s.substr(i+1),result,temp);
+            string subStr = s.substr(0, i+1);
+            if(isPalindrome(subStr)){
+                temp.push_back(subStr);
+                dfs(s.substr(i+1), result, temp);
                 temp.pop_back();
             }
         }
@@ -29,8 +31,9 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> result;
         vector<string> temp;
-        helper(s,result,temp);
-        
+
+        dfs(s, result, temp);
+
         return result;
     }
 };
