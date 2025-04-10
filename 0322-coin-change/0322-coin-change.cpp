063da812cd8bb078aeb0477b34325ee7af38dd41
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-04-10 15:07:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        if(amount == 0 || coins.size() == 0) return 0;
-        
-        // int dp[100] = {amount+1}; 
-        vector<int> dp(amount+1,amount+1);
-        dp[0] = 0;
-        
-        for(int i=0;i<coins.size();i++){
-            for(int j=coins[i];j<=amount;j++){
-                dp[j] = min(dp[j], dp[j-coins[i]]+1);
+        queue<pair<int,int>> q;
+        q.push({amount, 0});
+
+        while(!q.empty()){
+            auto [currAmount, count] = q.front();
+            q.pop();
+
+            if(currAmount == 0) return count;
+
+            for(auto coin:coins){
+                if(currAmount-coin >= 0){
+                    q.push({currAmount-coin, count+1});
+                }
             }
         }
-        
-        return dp[amount] == amount+1?-1:dp[amount];
+
+        return -1;
     }
 };
