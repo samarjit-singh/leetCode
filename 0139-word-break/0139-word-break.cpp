9421da908fbc5d9ac13b205c59e103e8b9551694
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-04-14 17:17:55 +0530

    Time: 0 ms (100%), Space: 10.3 MB (90.03%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    int dp[301];
-    int help(int i, string s,set<string>&wordDict){
-        if(i==s.size()) return 1;
-        string temp;
-        if(dp[i]!=-1){
-            cout<<dp[i]<<" ";
-            return dp[i];
-        } 
-        for(int j=i;j<s.size();j++)
-        {
-            temp += s[j];
-            if(wordDict.find(temp) != wordDict.end()){
-                if(help(j+1,s,wordDict)) return dp[i] = 1;
-            }
-        }
-        return dp[i] = 0;
-    }
-    
     bool wordBreak(string s, vector<string>& wordDict) {
-        set<string> st;
-        memset(dp,-1,sizeof dp);
-        for(auto a:wordDict) st.insert(a);
-        
-        return help(0,s,st);
+        vector<bool> dp(s.size() + 1, false);
+        dp[s.size()] = true;
+
+        for(int i=s.size()-1;i>=0;i--){
+            for(auto w:wordDict) {
+                if(i + w.size() <= s.size() && w == s.substr(i, w.size())) {
+                    dp[i] = dp[w.size() + i];
+                }
+                if(dp[i]) {
+                    break;
+                }
+            } 
+        }
+
+        return dp[0];
     }
 };
