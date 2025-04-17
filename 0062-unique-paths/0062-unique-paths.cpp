d02487242a68c822a228194b9e1432b189a6dda2
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-04-17 17:33:11 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    
-    int util(int i,int j,int n,int m,vector<vector<int>>& dp){
-        if(i>=n || j>=m) return 0;
-        if(i==n-1 && j == m-1) return 1;
-        
-        if(dp[i][j] != -1) 
-            return dp[i][j];
-        else {
-            return dp[i][j] = util(i+1,j,n,m,dp) + util(i,j+1,n,m,dp);
-        } 
-        
-        
+    int util(int m, int n, int row, int col) {
+        if(row >= m || col >= n) {
+            return 0;
+        }
+        // reached destination
+        if(row == m-1 && col == n-1) {
+            return 1;
+        }
+
+        int right = util(m, n, row, col+1);
+        int down = util(m, n, row+1, col);
+
+        return right+down;
     }
-    
+
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return util(0,0,n,m,dp);
+        vector<vector<int>> grid(m, vector<int> (n , 0));
+
+        return util(m, n, 0, 0);
     }
 };
