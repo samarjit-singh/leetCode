f85dde16f6c4d48f2c63af79e0565be796443de0
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-04-19 14:06:24 +0530

    Time: 39 ms (45.88%), Space: 42.7 MB (19.23%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
+    int OFFSET = 1000;
 
-    int backtrack(int sum, vector<int>& nums, int target, int index) {
-        if(index == nums.size()) {
-            return sum == target ? 1 : 0;
+    int backtrack(vector<int>& temp, vector<int>& nums, int target, int index,vector<vector<int>>& dp) {
+        if(index >= nums.size()) {
+            long long sumOfTemp = accumulate(temp.begin(), temp.end(), 0LL);
+            if(sumOfTemp == target) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        int sumSoFar = accumulate(temp.begin(), temp.end(), 0);
+        if(dp[index][sumSoFar + OFFSET] != -1) {
+            return dp[index][sumSoFar + OFFSET];
         }
 
-        int plus = backtrack(sum + nums[index], nums, target, index+1);
+        temp.push_back(nums[index]);
+        int plus = backtrack(temp, nums, target, index+1, dp);
 
-        int minus = backtrack(sum - nums[index], nums, target, index+1);
+        temp.pop_back();
+        temp.push_back(-nums[index]);
+        int minus = backtrack(temp, nums, target, index+1, dp);
 
-        return plus+minus;
+        temp.pop_back();
+
+        return dp[index][sumSoFar + OFFSET] = plus+minus;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        return backtrack(0, nums, target, 0);
+        vector<int> temp;
+        vector<vector<int>> dp(nums.size() + 1, vector<int>(2001, -1));
+        return backtrack(temp, nums, target, 0, dp);
     }
 };
