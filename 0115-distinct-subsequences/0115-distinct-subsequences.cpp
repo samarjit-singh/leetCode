cba2fe7d204640384fb952f0bab8a7d38b23f03e
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-04-23 12:40:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,26 +1,42 @@
 class Solution {
 public:
 
-    
-    int numDistinct(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        
-        vector<vector<double>> dp(n+1,vector<double> (m+1,0));
-        
-        for(int i=0;i<=n;i++) dp[i][0] = 1;
-        // for(int j=1;j<=m;j++) dp[0][j] = 0;
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i-1] == t[j-1]){
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-                } else {
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
+    int dfs(string s, string t, int index, string currStr) {
+        cout<<currStr<<endl;
+
+        if(currStr == t) {
+            return 1;
         }
-        
-        return (int)dp[n][m];
+
+        if(index >= s.size() || currStr.size() > t.size()) {
+            return 0; 
+        }
+
+        // not pick
+        int notpick = dfs(s, t, index+1, currStr);
+
+        // pick
+        int pick = dfs(s, t, index+1, currStr += s[index]);
+
+        return pick + notpick;
+    }
+
+    int numDistinct(string s, string t) {
+        int count = 0;
+        string currStr = "";
+        return dfs(s,t, 0, currStr);
     }
 };
+
+
+// pick r
+// notpick a
+// currStr = "r".  
+
+// pick a 
+// notpick a
+// currStr = "ra"
+
+// pick a
+// not pick
+// currStr = "a"
