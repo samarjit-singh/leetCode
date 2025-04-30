4cc2b9387a482327b4d2877fb26c7da9946f2503
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-04-30 11:34:41 +0530

    Time: 482 ms (5.14%), Space: 13.9 MB (39.42%) - LeetHub

---
+++ 2215-finding-3-digit-even-numbers/2215-finding-3-digit-even-numbers.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void backtrack(vector<int>& digits, set<int>& res, int digitIndex, int digit, vector<bool>& used) {
+        if(digitIndex == 3) {
+            if(digit % 2 == 0) {
+                res.insert(digit);
+            }
+            return;
+        }
+
+
+        for(int i=0;i<digits.size();i++) {
+            if(used[i]) continue; 
+
+            if(digitIndex == 0 && digits[i] == 0) continue;
+
+            used[i] = true;
+            backtrack(digits, res, digitIndex+1, digit * 10 + digits[i], used);
+            used[i] = false;
+        }
+    }
+
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        sort(digits.begin(), digits.end());
+        vector<bool> used(digits.size(), false);
+
+        set<int> res;
+        backtrack(digits, res, 0, 0, used);
+
+        vector<int> result(res.begin(), res.end());
+
+        return result;
+    }
+};
