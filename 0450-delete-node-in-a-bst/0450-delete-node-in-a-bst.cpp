5434de4a200b53d0175713e46fd29094b529aeb6
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-05-06 17:38:55 +0530

    Time: 0 ms (100%), Space: 34.3 MB (63.34%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -1,36 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if (root == nullptr) {
-            return root; // Base case: the tree is empty or the key is not found
+        if(root == NULL) {
+            return root;
         }
 
-        if (key < root->val) {
-            root->left = deleteNode(root->left, key); // Search in the left subtree
+        if(key < root->val) {
+            root->left = deleteNode(root->left, key);
         } else if (key > root->val) {
-            root->right = deleteNode(root->right, key); // Search in the right subtree
+            root->right = deleteNode(root->right, key);
         } else {
-            // Found the node to delete
-            if (root->left == nullptr) {
-                return root->right; // If the left child is null, return the right child
-            } else if (root->right == nullptr) {
-                return root->left; // If the right child is null, return the left child
+            if(!root->left && !root->right) {
+                return NULL;
             }
 
-            // Node has two children: Replace with the inorder successor (min value in the right subtree)
-            TreeNode* successor = findMin(root->right);
-            root->val = successor->val; // Replace the value
-            root->right = deleteNode(root->right, successor->val); // Delete the successor node
+            if(!root->left) {
+                return root->right;
+            } 
+            if(!root->right) {
+                return root->left;
+            }
+
+            TreeNode* temp = root->left;
+
+            while(temp->right) {
+                temp = temp->right;
+            }
+            root->val = temp->val;
+            root->left = deleteNode(root->left, temp->val);
         }
+
 
         return root;
-    }
-
-private:
-    TreeNode* findMin(TreeNode* node) {
-        while (node->left != nullptr) {
-            node = node->left; // Go to the leftmost node
-        }
-        return node;
     }
 };
