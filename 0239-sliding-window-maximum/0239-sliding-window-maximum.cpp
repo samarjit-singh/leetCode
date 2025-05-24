9bf493987eca74f9a2bbbe2f953d9fad2e3a0188
bravosamarjit <75423063+samarjit-singh@users.noreply.github.com>
2025-05-24 17:15:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int>v;
-        list<int>q;
-        int i=0;
-        int j=0;
-        while(j<nums.size()){
-            while(q.size()>0&&q.back()<nums[j]){
-                cout<<q.back()<<" ";
-                q.pop_back();
+        int n = nums.size();
+
+        vector<int> result;
+        int maxEle = INT_MIN;
+
+        for(int i=0;i<k;i++) {
+            if(nums[i] > maxEle) {
+                maxEle = nums[i];
             }
-            q.push_back(nums[j]);
-              if(j-i+1==k){
-                v.push_back(q.front());
-                if(q.front()==nums[i]){
-                    q.pop_front();
-                }
-                i++;
-            }
-            j++;
         }
-        return v;
+
+        result.push_back(maxEle);
+
+        for(int right=k;right<n;right++) {
+            int left = right - k + 1;
+            
+            if(nums[left-1] == maxEle) {
+                maxEle = *max_element(nums.begin() + left, nums.begin() + right + 1);
+            } else {
+                maxEle = max(maxEle, nums[right]);
+            }
+
+            result.push_back(maxEle);
+        }
+
+        return result;
     }
 };
